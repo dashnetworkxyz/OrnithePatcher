37ad3d6f39c151335c2aece960204278f08cf437
MasterDash5 <github@masterdash5.com>
2026-04-16 21:17:01 -0600

    alex arm position fix

---
+++ src/main/java/club/sk1er/patcher/mixins/bugfixes/modelfixes/PlayerModelMixin_FixedTransformations.java
@@ -0,0 +1,38 @@
+package club.sk1er.patcher.mixins.bugfixes.modelfixes;
+
+import net.minecraft.client.render.model.entity.HumanoidModel;
+import net.minecraft.client.render.model.entity.PlayerModel;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import xyz.dashnetwork.ornithepatcher.PatcherConfig;
+
+@Mixin(PlayerModel.class)
+public abstract class PlayerModelMixin_FixedTransformations extends HumanoidModel {
+
+    @Shadow
+    private boolean thinArms;
+
+    @ModifyConstant(method = "<init>", constant = @Constant(floatValue = 2.5F))
+    private float patcher$fixAlexArmHeight(float original) {
+        return PatcherConfig.FIXED_ALEX_ARMS.get() ? 2.0F : original;
+    }
+
+    /**
+     * @author asbyth
+     * @reason Resolve item positions being incorrect on Alex models (MC-72397)
+     */
+    @Overwrite
+    public void translateRightArm(float scale) {
+        if (thinArms) {
+            rightArm.x += 0.5F;
+            rightArm.render(scale);
+            rightArm.z -= 0.5F;
+        } else {
+            rightArm.render(scale);
+        }
+    }
+
+}
--- src/main/java/xyz/dashnetwork/ornithepatcher/PatcherConfig.java
+++ src/main/java/xyz/dashnetwork/ornithepatcher/PatcherConfig.java
@@ -1,15 +1,35 @@
 package xyz.dashnetwork.ornithepatcher;
 
+import net.ornithemc.osl.config.api.ConfigManager;
 import net.ornithemc.osl.config.api.ConfigScope;
 import net.ornithemc.osl.config.api.LoadingPhase;
 import net.ornithemc.osl.config.api.config.BaseConfig;
+import net.ornithemc.osl.config.api.config.option.BooleanOption;
 import net.ornithemc.osl.config.api.serdes.FileSerializerType;
 import net.ornithemc.osl.config.api.serdes.SerializerTypes;
 
 public class PatcherConfig extends BaseConfig {
 
+    private static PatcherConfig instance;
+
+    public static final BooleanOption FIXED_ALEX_ARMS;
+
+    static {
+        FIXED_ALEX_ARMS = new BooleanOption(
+                "Alex Arm Position",
+                "Resolve Alex-model arms being shifted down further than Steve-model arms.",
+                true
+        );
+    }
+
+    public static void initialize() {
+        if (instance == null)
+            ConfigManager.register(instance = new PatcherConfig());
+    }
+
     @Override
     public void init() {
+        registerOptions("Bug Fixes", FIXED_ALEX_ARMS);
     }
 
     @Override
