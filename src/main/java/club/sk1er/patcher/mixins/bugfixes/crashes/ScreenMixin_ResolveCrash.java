754055976324283d6f525dafa5614718826c7b3d
MasterDash5 <github@masterdash5.com>
2026-04-16 20:19:41 -0600

    oops

--- src/main/java/club/sk1er/patcher/mixins/bugfixes/crashes/ScreenMixin_ResolveCrash.java
+++ src/main/java/club/sk1er/patcher/mixins/bugfixes/crashes/ScreenMixin_ResolveCrash.java
@@ -10,7 +10,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Screen.class)
-public class GuiScreenMixin_ResolveCrash {
+public class ScreenMixin_ResolveCrash {
 
     @Shadow
     protected Minecraft minecraft;
--- src/main/java/club/sk1er/patcher/mixins/bugfixes/gui/InventoryMenuScreenMixin_Alpha.java
+++ src/main/java/club/sk1er/patcher/mixins/bugfixes/gui/InventoryMenuScreenMixin_Alpha.java
@@ -2,6 +2,7 @@ package club.sk1er.patcher.mixins.bugfixes.gui;
 
 import net.minecraft.client.gui.screen.inventory.menu.InventoryMenuScreen;
 import net.minecraft.client.render.platform.GlStateManager;
+import net.minecraft.inventory.slot.InventorySlot;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -22,10 +23,12 @@ public class InventoryMenuScreenMixin_Alpha {
 
     @Inject(
             method = "renderSlot",
-            at = @At(value = "INVOKE", target = "")
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/inventory/menu/InventoryMenuScreen;drawSprite(IILnet/minecraft/client/render/texture/TextureAtlasSprite;II)V")
     )
-    private void slot() {
-
+    private void slot(InventorySlot slot, CallbackInfo ci) {
+        GlStateManager.enableBlend();
+        GlStateManager.enableAlphaTest();
+        GlStateManager.blendFuncSeparate(770, 771, 1, 0);
     }
 
 }
