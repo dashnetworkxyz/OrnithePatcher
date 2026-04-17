51174e46a76496a045f6c1d7de2e2b579cac5de4
MasterDash5 <github@masterdash5.com>
2026-04-16 20:44:58 -0600

    finish bugfixes/gui mixins

---
+++ src/main/java/club/sk1er/patcher/mixins/bugfixes/gui/BookEditScreenMixin.java
@@ -0,0 +1,26 @@
+package club.sk1er.patcher.mixins.bugfixes.gui;
+
+import net.minecraft.client.gui.screen.inventory.BookEditScreen;
+import net.minecraft.client.render.platform.GlStateManager;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(BookEditScreen.class)
+public class BookEditScreenMixin {
+
+    @Inject(method = "render", at = @At("HEAD"))
+    private void alpha(int mouseX, int mouseY, float tickDelta, CallbackInfo ci) {
+        GlStateManager.enableBlend();
+        GlStateManager.enableAlphaTest();
+        GlStateManager.blendFuncSeparate(770, 771, 1, 0);
+    }
+
+    @Inject(method = "render", at = @At(value = "TAIL"))
+    private void end(int mouseX, int mouseY, float partialTicks, CallbackInfo ci) {
+        GlStateManager.disableAlphaTest();
+        GlStateManager.disableBlend();
+    }
+
+}
