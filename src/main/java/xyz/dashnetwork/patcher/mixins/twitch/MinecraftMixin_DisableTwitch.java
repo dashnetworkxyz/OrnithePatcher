49c8756e7d5812185cc00d5cfcb0c721aad3d4ea
MasterDash5 <github@masterdash5.com>
2026-04-18 17:45:33 -0600

    fix npe

--- src/main/java/xyz/dashnetwork/patcher/mixins/twitch/MinecraftMixin_DisableTwitch.java
+++ src/main/java/xyz/dashnetwork/patcher/mixins/twitch/MinecraftMixin_DisableTwitch.java
@@ -1,11 +1,14 @@
 package xyz.dashnetwork.patcher.mixins.twitch;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.options.KeyBinding;
 import net.minecraft.client.twitch.ErrorTwitchStream;
 import net.minecraft.client.twitch.TwitchStream;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(Minecraft.class)
 public class MinecraftMixin_DisableTwitch {
@@ -20,6 +23,17 @@ public class MinecraftMixin_DisableTwitch {
     @Overwrite
     private void initTwitchStream() {
         twitchStream = new ErrorTwitchStream(new UnsupportedOperationException("Twitch streaming is disabled."));
+    }
+
+    @Redirect(
+            method = "handleGuiKeyBindings",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/options/KeyBinding;getKeyCode()I")
+    )
+    private int patcher$disableTwitchKeybinds(KeyBinding keyBinding) {
+        if (keyBinding == null)
+            return 0;
+
+        return keyBinding.getKeyCode();
     }
 
 }
