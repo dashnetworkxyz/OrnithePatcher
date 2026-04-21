f64ab42e41d8f0a4cf4ea12c8475905f8f451462
MasterDash5 <github@masterdash5.com>
2026-04-21 15:03:34 -0600

    scale crosshair

--- src/main/java/club/sk1er/patcher/hooks/DebugCrosshairHook.java
+++ src/main/java/club/sk1er/patcher/hooks/DebugCrosshairHook.java
@@ -13,7 +13,7 @@ import java.awt.*;
 
 public class DebugCrosshairHook {
 
-    public static void renderDirections(float partialTicks, Minecraft minecraft) {
+    public static void renderDirections(Minecraft minecraft) {
         Window window = new Window(minecraft);
         double scale = window.getScale();
         Entity camera = minecraft.getCamera();
@@ -22,14 +22,14 @@ public class DebugCrosshairHook {
 
         GlStateManager.pushMatrix();
         GlStateManager.translatef((float)(window.getScaledWidth() / 2), (float)(window.getScaledHeight() / 2), 0);
-        GlStateManager.rotatef(camera.lastPitch + (camera.pitch - camera.lastPitch) * partialTicks, -1.0F, 0.0F, 0.0F);
-        GlStateManager.rotatef(camera.lastYaw + (camera.yaw - camera.lastYaw) * partialTicks, 0.0F, 1.0F, 0.0F);
+        GlStateManager.rotatef(camera.lastPitch + (camera.pitch - camera.lastPitch), -1.0F, 0.0F, 0.0F);
+        GlStateManager.rotatef(camera.lastYaw + (camera.yaw - camera.lastYaw), 0.0F, 1.0F, 0.0F);
         GlStateManager.scalef(-1.0F, -1.0F, -1.0F);
         GlStateManager.scaled(1 / scale, 1 / scale, 1 / scale);
         GlStateManager.disableTexture();
         GlStateManager.depthMask(false);
 
-        GL11.glLineWidth(2.0F);
+        GL11.glLineWidth(minecraft.height / 540.0F);
         buffer.begin(1, DefaultVertexFormat.POSITION_COLOR);
         drawLine(buffer, 38, Color.RED, 1, 0, 0);
         drawLine(buffer, 38, Color.BLUE, 0, 0, 1);
--- src/main/java/club/sk1er/patcher/mixins/bugfixes/optifine/GameRendererMixin_FixCrosshairZoom.java
+++ src/main/java/club/sk1er/patcher/mixins/bugfixes/optifine/GameRendererMixin_FixCrosshairZoom.java
@@ -28,24 +28,9 @@ public abstract class GameRendererMixin_FixCrosshairZoom {
             cancellable = true
     )
     private void patcher$cancelDebugCrosshairDraw(CallbackInfo ci) {
-        canDraw = true;
+        setupGuiState();
+        DebugCrosshairHook.renderDirections(minecraft);
         ci.cancel();
-    }
-
-    @Inject(
-            method = "render(FJ)V",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GameGui;render(F)V")
-    )
-    private void patcher$redrawDebugCrosshair(float partialTicks, long nanoTime, CallbackInfo ci) {
-        if (canDraw) {
-            setupGuiState();
-            DebugCrosshairHook.renderDirections(partialTicks, minecraft);
-        }
-    }
-
-    @Inject(method = "render(FJ)V", at = @At("TAIL"))
-    private void patcher$resetState(CallbackInfo ci) {
-        canDraw = false;
     }
 
 }
