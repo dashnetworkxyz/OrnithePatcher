56f08034bfa9850b7c088152c88965b71f204dcc
MasterDash5 <github@masterdash5.com>
2026-04-30 14:27:05 -0600

    shadow player instead

--- src/main/java/club/sk1er/patcher/mixins/features/MinecraftMixin_DebugCreativeSpectatorSwap.java
+++ src/main/java/club/sk1er/patcher/mixins/features/MinecraftMixin_DebugCreativeSpectatorSwap.java
@@ -13,8 +13,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(Minecraft.class)
 public class MinecraftMixin_DebugCreativeSpectatorSwap {
 
-    @Shadow
-    public Screen screen;
+    @Shadow public Screen screen;
+    @Shadow public LocalClientPlayerEntity player;
 
     @Inject(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/Minecraft;handleGuiKeyBindings()V"))
     private void patcher$f3n(CallbackInfo callback) {
@@ -22,8 +22,6 @@ public class MinecraftMixin_DebugCreativeSpectatorSwap {
             int pressed = Keyboard.getEventKey() == 0 ? Keyboard.getEventCharacter() + 256 : Keyboard.getEventKey();
 
             if (pressed == Keyboard.KEY_N && Keyboard.isKeyDown(Keyboard.KEY_F3)) {
-                LocalClientPlayerEntity player = Minecraft.getInstance().player;
-
                 if (player == null)
                     return;
 
