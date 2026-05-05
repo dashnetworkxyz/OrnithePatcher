312741597dff91e6a12ebb0e1bebaff5e747853e
MasterDash5 <github@masterdash5.com>
2026-05-05 10:21:15 -0600

    remove water overlay option

---
+++ src/main/java/club/sk1er/patcher/mixins/bugfixes/render/item/ItemInHandRendererMixin_RemoveWaterOverlay.java
@@ -0,0 +1,19 @@
+package club.sk1er.patcher.mixins.bugfixes.render.item;
+
+import net.minecraft.client.render.ItemInHandRenderer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import xyz.dashnetwork.patcher.Patcher;
+
+@Mixin(ItemInHandRenderer.class)
+public class ItemInHandRendererMixin_RemoveWaterOverlay {
+
+    @Inject(method = "renderInWaterEffect", at = @At("HEAD"), cancellable = true)
+    private void patcher$removeWaterOverlay(CallbackInfo ci) {
+        if (Patcher.get().config().options().removeWaterOverlay.get())
+            ci.cancel();
+    }
+
+}
--- src/main/java/xyz/dashnetwork/patcher/config/ConfigOptions.java
+++ src/main/java/xyz/dashnetwork/patcher/config/ConfigOptions.java
@@ -4,9 +4,12 @@ import java.util.concurrent.atomic.AtomicBoolean;
 
 public class ConfigOptions {
 
+    // Features
     public AtomicBoolean f3nKeybind = new AtomicBoolean(true);
     public AtomicBoolean borderlessFullscreen = new AtomicBoolean(false);
+    public AtomicBoolean removeWaterOverlay = new AtomicBoolean(false);
 
+    // Performance
     public AtomicBoolean batchModelRendering = new AtomicBoolean(true);
 
 }
