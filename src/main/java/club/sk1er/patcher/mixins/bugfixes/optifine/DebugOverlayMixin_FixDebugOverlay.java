fd2b056db615e15a7ab54af712f3adbf4cf63de9
MasterDash5 <github@masterdash5.com>
2026-05-05 08:50:01 -0600

    optfine f3 fixes

--- src/main/java/club/sk1er/patcher/mixins/bugfixes/optifine/DebugOverlayMixin_FixDebugOverlay.java
+++ src/main/java/club/sk1er/patcher/mixins/bugfixes/optifine/DebugOverlayMixin_FixDebugOverlay.java
@@ -7,7 +7,6 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-import java.lang.reflect.Method;
 import java.util.List;
 
 @Mixin(DebugOverlay.class)
