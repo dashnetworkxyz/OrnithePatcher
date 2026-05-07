0aa7518a78254088672b694926eee8691933b293
MasterDash5 <github@masterdash5.com>
2026-05-07 02:00:31 -0600

    miss a spot?

--- src/main/java/xyz/dashnetwork/patcher/config/screen/PatcherScreensOptionsScreen.java
+++ src/main/java/xyz/dashnetwork/patcher/config/screen/PatcherScreensOptionsScreen.java
@@ -21,7 +21,7 @@ public class PatcherScreensOptionsScreen extends AbstractOptionsScreen {
         addToggle("Extended Chat Length", """
                         Extended Chat Length
                          ON - Always use extended chat length
-                         AUTO - Only use on servers known to
+                         Auto - Only use on servers known to
                              support this and singleplayer
                          OFF - Use regular 100 chat length
                         Uses the 1.11 chat length of 256.
